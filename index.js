22c90d76565d963ec2dc1c70a7bc4a7f77560e4d
Dan Richards <danrich@google.com>
2022-12-30 08:31:20 +0100

    styling fixes and wiping form on submit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
-const fs = require("fs");
+
 const readline = require("readline");
 
 // var things = require("gameplay");
@@ -11,8 +11,10 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
-app.use(fs)
+
+
 app.use(express.static(__dirname+'/public'));
+
 app.listen(port, () => {
   console.log(`Running server on PORT ${port}...`);
 })
